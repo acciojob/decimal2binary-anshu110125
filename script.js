949003e7f3a48a7d9aab4c534998d08389dc10e2
Anshu Kumar <117300557+anshu110125@users.noreply.github.com>
2023-05-27 10:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function threeSum(arr, target) {
 // write your code here
+	function decimalToBinary(decimal) {
+  let binary = '';
+
+  // Edge case: handle input 0 separately
+  if (decimal === 0) {
+    return '0';
+  }
+
+  while (decimal > 0) {
+    // Append the remainder to the binary representation string
+    binary = (decimal % 2) + binary;
+    // Integer division by 2 to get the quotient
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
+}
+
+// Example usage:
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
+
   
 }
 
